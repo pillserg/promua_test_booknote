e30409428559d6bf28a7d4947cd57daa040d4cca
pill <pill.sv0@gmail.com>
2012-09-10 21:50:22 +0300

    bare models

--- booknote/models.py
+++ booknote/models.py
@@ -1,1 +1,26 @@
 from booknote import app, db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+
+    def __repr__(self):
+        return u'<User: {}'.format(self.username)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), index=True)
+
+    def __repr__(self):
+        return u'<Book: {}'.format(self.title)
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), index=True)
+
+    def __repr__(self):
+        return u'<Author: {}'.format(self.name)
+
