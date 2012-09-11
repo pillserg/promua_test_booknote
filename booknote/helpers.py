7b6d65dfda50d756652071928de5c1f6a80d5656
pill <pill.sv0@gmail.com>
2012-09-11 15:43:47 +0300

    some inputs styling and mock authors on book input

--- booknote/helpers.py
+++ booknote/helpers.py
@@ -6,6 +6,8 @@ def to_json(obj):
     flask.jsonify converts everything to dict
     this is thinner
     """
+    # I really think there are something built in like this, 
+    # but unfortunately i couldn't find it.
     return current_app.response_class(
         json.dumps(obj, indent=None if request.is_xhr else 2),
         mimetype='application/json')
---
+++ booknote/static/js/authors_autocomplite.js
@@ -0,0 +1,8 @@
+// requires jQuery-Tokeninput
+$(document).ready(function(){
+    var config = {
+        
+    }
+    $('input#authors').tokenInput('/authors_autocomplite', config)
+    
+});
